707a0c0ca20708a4289eb3d8b3b60860748664f9
Ricozero <735784247@qq.com>
2021-06-19 00:51:19 +0800

    impl. SNICO

--- snic.py
+++ snic.py
@@ -21,6 +21,7 @@ def find_seeds(width, height, numk):
         for x in range(0, xsteps):
             seeds[y * xsteps + x, 0] = halfstep + y * gridstep
             seeds[y * xsteps + x, 1] = halfstep + x * gridstep
+    print('gridstep: %d' % gridstep)
     return numk_new, seeds
 
 def snic(img, numk, compactness):
@@ -74,6 +75,56 @@ def snic(img, numk, compactness):
     f.close()
     return labels
 
+def snico(img, numk):
+    # reduce range of lightness, or else boundaries would be wavy
+    img[:,:,0] = img[:,:,0] * 100 / 255
+
+    # constants
+    h = img.shape[0]
+    w = img.shape[1]
+    numk, seeds = find_seeds(w, h, numk)
+    di = [-1, 0, 1, 0, -1, 1, 1, -1]
+    dj = [0, -1, 0, 1, -1, -1, 1, 1]
+    connectivity = 4
+    area = h * w / numk
+    DC = 20 # default compactness
+
+    maxcdsq = np.zeros(numk) # max color distances squared
+    for iter in range(2):
+        labels = -1 * np.ones((h, w), np.int)   # pixel labels
+        kf = np.zeros((numk, 5), np.double)     # sp features: l,a,b,i,j
+        ks = np.zeros(numk, np.int)             # sp sizes
+        heap = []
+        for k in range(numk):
+            heap.append((0, k, seeds[k, 0], seeds[k, 1]))
+        heapq.heapify(heap)
+        while len(heap) > 0:
+            d, k, i, j = heapq.heappop(heap)
+            if labels[i, j] < 0: # takes longer time if removed
+                labels[i, j] = k
+                l, a, b = img[i, j, :].tolist()
+                kf[k, :] = (kf[k, :] * ks[k] + (l, a, b, i, j)) / (ks[k] + 1)
+                ks[k] += 1
+                for n in range(connectivity):
+                    ii = i + di[n]
+                    jj = j + dj[n]
+                    if ii < 0 or jj < 0 or ii >= h or jj >= w:
+                        continue
+                    if labels[ii, jj] < 0:
+                        pf = (*(img[ii, jj, :].tolist()), ii, jj) # pixel features
+                        dist = kf[k, :] - pf
+                        dist = [d * d for d in dist]
+                        colordist = sum(dist[0:3])
+                        spacedist = sum(dist[3:5])
+                        if iter == 0:
+                            slicdist = colordist + DC * DC * spacedist / area
+                            if maxcdsq[k] < colordist:
+                                maxcdsq[k] = colordist
+                        else:
+                            slicdist = colordist + maxcdsq[k] * spacedist / area
+                        heapq.heappush(heap, (slicdist, k, ii, jj))
+    return labels
+
 #%% Visualization Functions
 def show_seeds(img, numk):
     h = img.shape[0]
@@ -99,16 +150,20 @@ def export_labels_mat():
     labels = snic(img_lab, 200, 20)
     savemat('matlab/reimpl_labels.mat', {"reimpl_labels":labels})
 
-def test():
-    img = cv2.imread('example.jpg')
+def test(zero_param=False):
+    img = cv2.imread('bee.jpg')
+    #img = img[0:100,0:100]
     print('%dx%d' % (img.shape[0], img.shape[1]))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
     t0 = time()
-    labels = snic(img_lab, 200, 20)
+    if zero_param:
+        labels = snico(img_lab, 200)
+    else:
+        labels = snic(img_lab, 200, 20)
     print('Time used: %.3fs' %(time() - t0))
     show_bounaries(img, labels)
 
 if __name__ =='__main__':
-    export_labels_mat()
+    test(True)
     # TODO: visualize algorithm process
